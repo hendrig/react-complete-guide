a9520e75bd2c2ca8e58a387e66f9abe8ce31f223
Hendrig Wernner Maus Santana Goncalves <hendrig.goncalves@ambevtech.com.br>
2022-04-25 15:48:23 -0300

    Add new features

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,15 +1,32 @@
+import React, { useState } from 'react';
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card"
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const [oldTitle, setOldTitle] = useState(title);
+  
+  const clickHandler = () =>{
+    setOldTitle(title);
+    setTitle('Updated!');
+    console.log("title changes");
+  }
+
+  const unclickHandler = () =>{
+    setTitle(oldTitle);
+    console.log("title changes");
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2> {props.title}</h2>
+        <h2> {title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
+      <button onClick={unclickHandler}>Revert</button>
     </Card>
   );
 }
