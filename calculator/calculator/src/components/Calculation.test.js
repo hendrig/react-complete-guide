02bb550e5ab5e65f9989a77e4bd8851ef012dc01
Hendrig Wernner Maus Santana Goncalves <hendrig.goncalves@ambevtech.com.br>
2022-04-27 17:59:14 -0300

    Add rule explanation on test

--- calculator/calculator/src/components/Calculation.test.js
+++ calculator/calculator/src/components/Calculation.test.js
@@ -4,3 +4,9 @@ it('sum', () =>{
     let c = new Calculation();
     expect(c.Sum(1, 2)).toEqual(3);
 })
+
+/*
+n = número de dias seguidos
+declinacao = 23.45 * sen((360/365)*(284 + n))
+TD = (2/15) * arccos(-tan(latitude)*tan(declinacao))
+ */
